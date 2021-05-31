d32fc9c955d9908457c5f6948c41e3a84df93ac9
Chris Grigg <grigg_chris@cat.com>
2021-05-31 17:17:43 +1000

    Create release retention algorithm with test.

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -1,31 +1,94 @@
-import { Delays, greeter } from '../src/main';
+import { releasesToKeep } from '../src/main';
 
-describe('greeter function', () => {
-  // Read more about fake timers
-  // http://facebook.github.io/jest/docs/en/timer-mocks.html#content
-  jest.useFakeTimers();
+describe('release retention function', () => {
+    it('releasesToKeep to be defined', () => {
+        expect(releasesToKeep).toBeDefined();
+    });
 
-  const name = 'John';
-  let hello: string;
+    it('Keep 1 release for every project/environment combination', () => {
+        expect(releasesToKeep(1)).toStrictEqual([
+            {
+                Id: 'Release-1',
+                ProjectId: 'Project-1',
+                Version: '1.0.0',
+                Created: '2000-01-01T09:00:00'
+            },
+            {
+                Id: 'Release-2',
+                ProjectId: 'Project-1',
+                Version: '1.0.1',
+                Created: '2000-01-02T09:00:00'
+            },
+            {
+                Id: 'Release-7',
+                ProjectId: 'Project-2',
+                Version: '1.0.3',
+                Created: '2000-01-02T12:00:00'
+            },
+            {
+                Id: 'Release-6',
+                ProjectId: 'Project-2',
+                Version: '1.0.2',
+                Created: '2000-01-02T09:00:00'
+            },
+            {
+                Id: 'Release-8',
+                ProjectId: 'Project-3',
+                Version: '2.0.0',
+                Created: '2000-01-01T09:00:00'
+            }
+        ]);
+    });
 
-  // Act before assertions
-  beforeAll(async () => {
-    const p: Promise<string> = greeter(name);
-    jest.runOnlyPendingTimers();
-    hello = await p;
-  });
+    it('Keep 10 releases for every project/environment combination', () => {
+        // this should max out to all valid releases as there aren't any more than 4 releases
+        // per project/environment combination with the Releases.json dataset
+        expect(releasesToKeep(10)).toStrictEqual([
+            {
+                Id: 'Release-1',
+                ProjectId: 'Project-1',
+                Version: '1.0.0',
+                Created: '2000-01-01T09:00:00'
+            },
+            {
+                Id: 'Release-2',
+                ProjectId: 'Project-1',
+                Version: '1.0.1',
+                Created: '2000-01-02T09:00:00'
+            },
+            {
+                Id: 'Release-7',
+                ProjectId: 'Project-2',
+                Version: '1.0.3',
+                Created: '2000-01-02T12:00:00'
+            },
+            {
+                Id: 'Release-6',
+                ProjectId: 'Project-2',
+                Version: '1.0.2',
+                Created: '2000-01-02T09:00:00'
+            },
+            {
+                Id: 'Release-5',
+                ProjectId: 'Project-2',
+                Version: '1.0.1-ci1',
+                Created: '2000-01-01T10:00:00'
+            },
+            {
+                Id: 'Release-8',
+                ProjectId: 'Project-3',
+                Version: '2.0.0',
+                Created: '2000-01-01T09:00:00'
+            }
+        ]);
+    });
 
-  // Assert if setTimeout was called properly
-  it('delays the greeting by 2 seconds', () => {
-    expect(setTimeout).toHaveBeenCalledTimes(1);
-    expect(setTimeout).toHaveBeenLastCalledWith(
-      expect.any(Function),
-      Delays.Long,
-    );
-  });
+    it('Keep 0', () => {
+        expect(releasesToKeep(0)).toStrictEqual([]);
+    });
 
-  // Assert greeter result
-  it('greets a user with `Hello, {name}` message', () => {
-    expect(hello).toBe(`Hello, ${name}`);
-  });
+    it('Keep -1', () => {
+        expect(releasesToKeep(-1)).toStrictEqual([]);
+    });
+
 });
--- src/main.ts
+++ src/main.ts
@@ -1,32 +1,106 @@
-/**
- * Some predefined delays (in milliseconds).
- */
-export enum Delays {
-  Short = 500,
-  Medium = 2000,
-  Long = 5000,
+// Aim of this is to create an array of dictionaries that contain FIFO queues (arrays)
+// of each environment/project combination. Then loop through this array taking n
+// releases off each queue.
+//
+// [
+//      'Project-1_Environment-1': [{ Release_1 ... }, { Release_5 ...}],
+//      'Project-1_Environment-2': [{ Release_6 ... }, { Release_8 ...}],
+//      'Project-3_Environment-1': [{ Release_3 ... }, { Release_2 ...}],
+// ]
+//
+// TODO WITH MORE TIME
+//  - add logging utility component that records console.log and sends to DB or 3rd party service
+//  - add more detailed logging such as 'it was the 2nd release added to environment 1 which is "Staging"'
+//  - add unit tests with more data and broken data
+//
+
+import * as deploymentsJSON from "../data/Deployments.json";
+import * as releasesJSON from "../data/Releases.json";
+
+interface Release {
+    Id: string;
+    ProjectId: string;
+    Version: string;
+    Created: string;
 }
 
-/**
- * Returns a Promise<string> that resolves after given time.
- *
- * @param {string} name - A name.
- * @param {number=} [delay=Delays.Medium] - Number of milliseconds to delay resolution of the Promise.
- * @returns {Promise<string>}
- */
-function delayedHello(
-  name: string,
-  delay: number = Delays.Medium,
-): Promise<string> {
-  return new Promise((resolve: (value?: string) => void) =>
-    setTimeout(() => resolve(`Hello, ${name}`), delay),
-  );
+interface Deployment {
+    Id: string;
+    ReleaseId: string;
+    EnvironmentId: string;
+    DeployedAt: string;
 }
 
-// Below are examples of using ESLint errors suppression
-// Here it is suppressing missing return type definitions for greeter function
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+function getUniqueListBy(arr: any[], key: string): Deployment[] {
+    // a different (es6+) way of creating a Map than the
+    // 'releasesToKeep' function below as this is a showcase
+    return [...new Map(arr.map(item => [item[key], item])).values()]
+}
 
 // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
-export async function greeter(name: string) {
-  return await delayedHello(name, Delays.Long);
+export function releasesToKeep(n: number): Release[] {
+    if (!n || n <= 0) {
+        console.log('n is not valid');
+        return [];
+    }
+    const deployments = deploymentsJSON['default'];
+    const deploymentsSorted: Deployment[] = deployments.sort((a: Deployment, b: Deployment) => Date.parse(a.DeployedAt) - Date.parse(b.DeployedAt));
+    const deploymentsCleaned: Deployment[] = getUniqueListBy(deploymentsSorted, 'ReleaseId');
+    const releases = releasesJSON['default'];
+
+    // first create an object with keys that correspond to releases
+    // the alternative is a nested loop which would be 0(n*m)
+    const deploymentsMap = {};
+    for (const deployment of deploymentsCleaned) {
+        const key = deployment.ReleaseId;
+        if (!deploymentsMap[key]) {
+            deploymentsMap[key] = [];
+        }
+        deploymentsMap[key].push(deployment);
+    }
+
+    // go through each key in the deployments map that
+    // correspond to the releases array 'Id'
+    const releasesMap = {};
+    for (const release of releases) {
+        const currentDeployments = deploymentsMap[release.Id];
+        if (currentDeployments) {
+            for (const currentDeployment of currentDeployments) {
+                if (release.Id === currentDeployment.ReleaseId) {
+                    // create an FIFO queue based on object key of project ID combined with environment ID
+                    const key = `${release.ProjectId}_${currentDeployment.EnvironmentId}`;
+                    if (!releasesMap[key]) {
+                        releasesMap[key] = [];
+                    }
+                    // unshift will make this a FIFO queue
+                    // this could be another component such as 'queue' but
+                    // perhaps that's overkill for such a small project
+                    releasesMap[key].unshift(release);
+                }
+            }
+        }
+    }
+
+    // now loop through the releases map object one by one
+    // then take off a specific amount of releases from each queue
+    const releasesToKeep: Release[] = [];
+    for (const key in releasesMap) {
+        let counter = 0;
+        const current = releasesMap[key];
+        for (let i = 0; i < current.length; i++) {
+            if (n >= counter) {
+                releasesToKeep.push(current[i]);
+                counter++;
+
+                const releaseId = current[i].Id;
+                const environmentId = key.split('_')[1];
+                console.log(`${releaseId} kept because it was the most recently deployed to ${environmentId}`);
+            } else {
+                i++;
+            }
+        }
+    }
+
+    return releasesToKeep;
 }
